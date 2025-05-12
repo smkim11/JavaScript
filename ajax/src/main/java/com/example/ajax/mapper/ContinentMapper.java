d737facbeb9b032cc8c8e1a7fa1c50c437a3f72d
kim <smkim000721@naver.com>
2025-05-12 17:23:53 +0900

    대륙,나라,도시 가져오는 쿼리 생성

---
+++ ajax/src/main/java/com/example/ajax/mapper/ContinentMapper.java
@@ -0,0 +1,13 @@
+package com.example.ajax.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ContinentMapper {
+	List<Map<String,Object>> selectContinentList(); 
+	List<Map<String,Object>> selectCountryList(int continentNo); 
+	List<Map<String,Object>> selectCityList(int countryNo); 
+}
